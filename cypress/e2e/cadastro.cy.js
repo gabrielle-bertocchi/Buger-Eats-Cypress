a75acd62d26f42f7e7dcc7e218a51d8f58967568
Gabrielle Bertocchi Sales <99894790+gabrielle-bertocchi@users.noreply.github.com>
2023-05-17 13:03:31 +0100

    Validacao do checkpoint

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('cadastro', () => {
+  it('Usuário deve se tornar um entregador', () => {
+    cy.viewport(1440, 900)
+    cy.visit('https://buger-eats.vercel.app/')
+    cy.get('a[href="/deliver"]').click()
+    cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
+    
+  });
+ 
 })
